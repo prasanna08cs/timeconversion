b1875950cc229c8d34500204aef7a9762c637d22
prasanna08cs <prasanna08cs@gmail.com>
2018-10-26 11:12:43 +0530

    Create timeconversion.js

---
+++ timeconversion.js
@@ -0,0 +1,12 @@
+let type ="am";
+let time=7;
+let t4='';
+  if(type =='am'){
+    console.log(time+type);
+  }else{
+     if(time ==12){
+       console.log(time+type);
+     } else {
+       console.log((12+time)+type);
+     }
+  }
